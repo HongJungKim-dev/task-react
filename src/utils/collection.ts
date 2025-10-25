7e6aded5438ba7d962a76e0184648e14d9b178c3
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2025-10-25 23:23:13 +0900

    feat: TODO 01 완료

--- src/utils/collection.ts
+++ src/utils/collection.ts
@@ -24,7 +24,7 @@ export function countDone(tasks: SimpleTask[]): Counters {
 export function pluckTitles(tasks: SimpleTask[]): string[] {
   // 예: [{title: 'a'}, {title: 'b'}] -> ['a','b']
   // 구현하세요.
-  return [] as string[] // <- 수정 필요
+  return tasks.map(({ title }) => title) // <- 수정 필요
 }
 
 // TODO 02: (기초) priority 가 'high' > 'medium' > 'low' 순으로 정렬되도록 구현하세요.
